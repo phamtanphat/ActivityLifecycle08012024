81acb7948aa1e5df384b66c80a27617f47b57f12
phatdroid94 <phatdroid94@gmail.com>
2024-07-15 19:01:50 +0700

    launch mode: Single top

--- app/src/main/java/com/example/activitylifecycle08012024/MainActivity3.kt
+++ app/src/main/java/com/example/activitylifecycle08012024/MainActivity3.kt
@@ -1,13 +1,18 @@
 package com.example.activitylifecycle08012024
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity3 : AppCompatActivity() {
+
+    private lateinit var btnNavigateMain3: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,7 +22,20 @@ class MainActivity3 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        btnNavigateMain3 = findViewById(R.id.button_navigate_main3)
+        btnNavigateMain3.setOnClickListener {
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
+
         Log.d("pphat", "Main3: onCreate")
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Log.d("pphat", "Main3: onNewIntent")
     }
 
     override fun onStart() {
